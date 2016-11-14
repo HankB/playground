5cfd32de619c2e3123f422d180386527d6d7899e
Hank Barta <hbarta@gmail.com>
2016-11-13 20:48:50 -0600

    Send UDP packet

--- udp-send.go
+++ udp-send.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-    "os"
-    "fmt"
-    "strconv"
-    )
+	"fmt"
+	"net"
+	"os"
+	"strconv"
+)
 
 func usage() {
 	fmt.Println("Usage:", os.Args[0], "nnnn")
@@ -12,14 +13,45 @@ func usage() {
 	os.Exit(1)
 }
 
+/* A Simple function to verify error */
+/* copied from https://varshneyabhi.wordpress.com/2014/12/23/simple-udp-clientserver-in-golang/ */
+func checkErr(err error) {
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(0)
+	}
+}
 func main() {
 	if len(os.Args) != 2 {
 		usage()
 	}
-        port, err := strconv.Atoi(os.Args[1])
-        if err != nil || port <= 0 || port > 0xFFFF {
-            fmt.Println("Cannot interpret", os.Args[1], "as a port")
-            usage()
-        }
-        fmt.Println("port:", port)
+	port, err := strconv.Atoi(os.Args[1])
+	if err != nil || port <= 0 || port > 0xFFFF {
+		fmt.Println("Cannot interpret", os.Args[1], "as a port")
+		usage()
+	}
+	fmt.Println("port:", port)
+        server := fmt.Sprintf("127.0.0.1:%d", port)
+
+	// get server handle
+	ServerAddr, err := net.ResolveUDPAddr("udp", server)
+	checkErr(err)
+
+	// get our handle
+	LocalAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:0")
+	checkErr(err)
+
+        // establish connection
+	conn, err := net.DialUDP("udp", LocalAddr, ServerAddr)
+	checkErr(err)
+
+        defer conn.Close()
+
+        msg := "Hello UDP World!\n"
+        buf := []byte(msg)
+        _,err = conn.Write(buf)
+	checkErr(err)
+
+        fmt.Println("msg sent, Sir!")
+
 }
