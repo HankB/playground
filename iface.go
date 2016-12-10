534d989066596d705adf22492fc194166d58ea09
Hank Barta <hbarta@gmail.com>
2016-12-10 16:33:27 -0600

    added function to return broadcast addresses

--- iface.go
+++ iface.go
@@ -7,6 +7,34 @@ import (
 	"strings"
 )
 
+func GetBroadcastIPs(includeIPV6 bool) ([]net.IP, error) {
+	addrs, err := net.InterfaceAddrs()
+	var bcast_addrs []net.IP
+	if err != nil {
+		return nil, err
+	} else {
+		for _, addr := range addrs {
+			if strings.Compare(addr.String(), "127.0.0.1/8") == 0 ||
+				strings.Compare(addr.String(), "::1/128") == 0 {
+			} else {
+				addrIP, ipNet, _ := net.ParseCIDR(addr.String())
+				bcast := ipNet.IP
+				for i := range ipNet.Mask {
+					bcast[i] |= ^ipNet.Mask[i]
+				}
+				if addrIP.DefaultMask() != nil {
+					bcast_addrs = append(bcast_addrs, bcast)
+					// fmt.Println("IPV4 ", addr, "ip is", addrIP, "net is", ipNet.IP, "mask is", ipNet.Mask, "bcast", bcast)
+				} else if includeIPV6 {
+					bcast_addrs = append(bcast_addrs, bcast)
+					// fmt.Println("IPV6 ", addr, "ip is", addrIP, "net is", ipNet.IP, "mask is", ipNet.Mask, "bcast", bcast)
+				}
+			}
+		}
+	}
+return bcast_addrs, nil
+}
+
 func main() {
 	addrs, err := net.InterfaceAddrs()
 	if err != nil {
@@ -32,4 +60,8 @@ func main() {
 			}
 		}
 	}
+	ba, _ := GetBroadcastIPs(true)
+	fmt.Println("From get_broadcast_addrs", ba)
+	ba, _ = GetBroadcastIPs(false)
+	fmt.Println("From get_broadcast_addrs w/out IPV6", ba)
 }
